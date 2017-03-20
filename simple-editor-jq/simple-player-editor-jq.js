9f5a42f24a5b8cdbcd6b2419b1b768ea923c5108
ChanJing <chanjing910514@gmail.com>
2017-03-20 16:41:52 +1300

    Add Chpater & !!! AUTO SLIDEUP and SLIDEDOWN Function
    $(this).next('div.panel').slideToggle('slow');
    
    if($(this).next('div.panel').attr('style')==="display: none;"){
    $('.panel').slideUp('slow');
    $(this).next('div.panel').slideDown('slow');
    }else{
    $(this).next('div.panel').slideUp('slow');
    }

--- simple-editor-jq/simple-player-editor-jq.js
+++ simple-editor-jq/simple-player-editor-jq.js
@@ -9,6 +9,7 @@ var $addChapter = $('#addChapter');
 $('#startEndSegmentButton').click(function(event) {
 
 	if($(this).text() === "Start Segment"){
+		$('.panel').slideUp('slow');
 		var $segmentTemplate = $('#segmentTemplate').html();
 		$('#segmentList').append($segmentTemplate);
 		addChapter.disabled = false;
@@ -23,8 +24,34 @@ $('#startEndSegmentButton').click(function(event) {
 
 $('#addChapter').click(function(event) {
 	var $lastSegment = $('#segmentList').children().last()[0];
-	
+	var $chapterTemplate = $('#chapterTemplate')[0];
+	var $lastChapterInfoList = $lastSegment.getElementsByClassName("chapter-info-list")[0];
+	$lastChapterInfoList.appendChild($chapterTemplate.content.cloneNode(true));
+
+	console.log($lastChapterInfoList);
+	console.log($chapterTemplate);
 });
+
+
+// $(".flip").click(function(event) {
+// 	console.log($(this));
+
+$(document).on("click" ,".flip",function() {
+	if($(this).next('div.panel').attr('style')==="display: none;"){
+		$('.panel').slideUp('slow');
+		$(this).next('div.panel').slideDown('slow');
+	}else{
+		$(this).next('div.panel').slideUp('slow');
+	}
+	
+
+
+	/*
+	@IMPORTANT
+	$(this).next('div.panel').slideToggle('slow');*/
+
+});
+
 
 
 
